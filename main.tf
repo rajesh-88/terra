0bb22cf1249cc037550d3e5dd88816eb78ab4bd5
rajesh-88 <r68754220@gmail.com>
2025-01-10 22:57:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "ap-south-2"
 }
 
 resource "aws_instance" "one" {
-ami = "ami-08a2b7219e5017d9e"
+ami = "ami-02fc7136ff51e18f2"
 instance_type = "t3.micro"
 tags = {
 Name = "rajesh-server"
